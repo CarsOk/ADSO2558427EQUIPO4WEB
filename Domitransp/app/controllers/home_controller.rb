9a128a7864bf5a6005ee008c10874afab6a32bae
juanhsd <110951069+juanhsd@users.noreply.github.com>
2023-06-16 01:58:30 -0500

    COnfiguracion de semilla, diseños nuevos, ruta de descargar pdf

--- Domitransp/app/controllers/home_controller.rb
+++ Domitransp/app/controllers/home_controller.rb
@@ -1,10 +1,13 @@
 class HomeController < ApplicationController
- def index
-     @mainTitle = "Welcome to Inspinia Rails Seed Project" 
-     @mainDesc = "It is an application skeleton for a typical Ruby on Rails web app. You can use it to quickly bootstrap your webapp projects and dev/prod environment."
-   end
+def index
+    @mainTitle = "Welcome to Inspinia Rails Seed Project" 
+    @mainDesc = "It is an application skeleton for a typical Ruby on Rails web app. You can use it to quickly bootstrap your webapp projects and dev/prod environment."
+    @orders = Order.last
+    
 
-   def minor
-   end
+end
+
+def minor
+end
 
 end
--- Domitransp/app/models/user.rb
+++ Domitransp/app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   belongs_to :company
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable, :validatable
 end
