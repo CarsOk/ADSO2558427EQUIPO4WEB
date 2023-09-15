1c1d4bb470cd190d3ab0401ad4bb7835a216621f
JuanCito16 <juanvides112011@gmail.com>
2023-09-15 15:40:07 -0500

    se agrego opcion de excel

--- Domitransp/config/routes.rb
+++ Domitransp/config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :orders do
     collection do
       get 'index'
+      get 'generar_pdf'
+      get 'export_to_excel', defaults: { format: :xlsx }
     end
   end
   devise_for :users
