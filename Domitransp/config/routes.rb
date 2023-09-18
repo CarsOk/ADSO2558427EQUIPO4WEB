a22049c8c36205683cd20d7d478a12fe9bc4c963
GoJhon <jsantander1219@gmail.com>
2023-09-18 16:27:05 +0000

    Colocando mapeo de metodo con match

--- Domitransp/config/routes.rb
+++ Domitransp/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resources :orders do
     collection do
-      get 'index'
+      match 'index', via: [:get, :post]
       get 'generar_pdf'
       get 'export_to_excel', defaults: { format: :xlsx }
     end
