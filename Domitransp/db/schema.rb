add2aaf09c3ad0c8076e7b561c70ff9947315af1
JuanCito16 <juanvides112011@gmail.com>
2023-10-09 14:34:32 -0500

    resolver conflicto del schema

--- Domitransp/db/schema.rb
+++ Domitransp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_06_053018) do
+ActiveRecord::Schema.define(version: 2023_10_08_160857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "adminpack"
@@ -112,6 +112,8 @@ ActiveRecord::Schema.define(version: 2023_10_06_053018) do
     t.string "code"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "estado", default: "En espera"
+    t.string "rol", default: "U"
   end
 
   create_table "responses", force: :cascade do |t|
