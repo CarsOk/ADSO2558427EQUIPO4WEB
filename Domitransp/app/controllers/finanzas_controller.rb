c8e46c6007c6fa0f33b93a2f53425731a7b4a645
JuanCito16 <juanvides112011@gmail.com>
2023-10-11 22:51:16 -0500

    Mis gastos

--- Domitransp/app/controllers/finanzas_controller.rb
+++ Domitransp/app/controllers/finanzas_controller.rb
@@ -1,5 +1,31 @@
 class FinanzasController < ApplicationController
+  before_action :authenticate_user!
+
   def index
-    @orders = Order.all.order(:fecha)  
+    if current_user.admin?
+      @orders = Order.all.order(:fecha)
+      if params[:fecha_desde].present?
+        fecha_desde = Date.parse(params[:fecha_desde])
+        @orders = @orders.where(fecha: fecha_desde.beginning_of_day..fecha_desde.end_of_day)
+      end
+      if params[:empresa].present?
+        @orders = @orders.joins(:company).where('companies.razon_social LIKE ?', "%#{params[:empresa]}%")
+      end
+    else
+      @orders = Order.where(company_id: current_user.company_id)
+      @orders = @orders.where(consecutivo: params[:consecutivo]) if params[:consecutivo].present?
+      @orders = filtro(@orders)
+    end
+  end
+
+  private
+
+  def filtro(query)
+    if params[:fecha_desde].present? && params[:fecha_hasta].present?
+      fecha_desde = Date.parse(params[:fecha_desde])
+      fecha_hasta = Date.parse(params[:fecha_hasta])
+      query = query.where(fecha: fecha_desde..fecha_hasta)
+    end
+    query
   end
 end
